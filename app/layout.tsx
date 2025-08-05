a47e6573a631e0a3aa2c89c51430cbff6be9ddee
omarbramirez <omar.ramirez94@hotmail.es>
2025-08-05 15:07:04 -0600

    google fonts fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { IBM_Plex_Sans, EB_Garamond } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+export const ibm = IBM_Plex_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'], // según tus necesidades
+  display: 'swap',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+export const garamond = EB_Garamond({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
@@ -24,9 +26,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+     <body className={`${ibm.className} ${garamond.className} antialiased`}>
+
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    UWU
+    <h1>HOLA LOLA</h1>
+    <p>Be a body</p>
     </>
   );
 }
