1012060c7d9813210e898db60c0fa5badf095b02
omarbramirez <omar.ramirez94@hotmail.es>
2026-06-15 07:18:46 -0600

    CVs and Footer updated

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,39 +1,61 @@
-
 import { assets } from "../assets/assets";
 import Image from "next/image";
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 import { SectionProps } from "../../types/forms";
 
+const Footer: React.FC<SectionProps> = ({ isDarkMode }) => {
+  const t = useTranslations("Footer");
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image
+          src={isDarkMode ? assets.logo_dark : assets.logo}
+          alt="Logotipo Omar B Ramirez"
+          className="w-66 mx-auto"
+        />
 
-const Footer:React.FC<SectionProps> = ({isDarkMode}) => {
-    const t = useTranslations('Footer')
-    return (
-        <div className="mt-20">
-            <div className="text-center">
-                <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='Logotipo Omar B Ramirez' className='w-66 mx-auto' />
-
-                <div className="w-max flex items-center gap-2 mx-auto">
-                    <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='Icono de correo electronico' className="w-6" />
-                    omar.ramirez94@hotmail.es
-                </div>
-            </div>
-
-            <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-                <p>&copy; {new Date().getFullYear()} Omar B Ramírez. {t('copyright')}</p>
-                <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
-                    <li>
-                        <a target="_blank" href="https://github.com/settings/profile">GitHub</a>
-                    </li>
-                    <li>
-                        <a target="_blank" href="https://dev.to/omarbramirez">Dev.to</a>
-                    </li>
-                    <li>
-                        <a target="_blank" href="https://www.linkedin.com/in/omar-b-ramirez/">LinkedIn</a>
-                    </li>
-                </ul>
-            </div>
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image
+            src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
+            alt="Icono de correo electronico"
+            className="w-6"
+          />
+          omar.ramirez94@hotmail.es
         </div>
-    )
-}
+      </div>
+
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>
+          &copy; {new Date().getFullYear()} Omar B Ramírez. {t("copyright")}
+        </p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/settings/profile">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://gitlab.com/omarbramirez">
+              GitLab
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://dev.to/omarbramirez">
+              Dev.to
+            </a>
+          </li>
+          <li>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/omar-b-ramirez/"
+            >
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
